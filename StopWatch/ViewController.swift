84e14ab19f7851056bce8421a66e0d4f21617c1c
choy1379 <choy1379@gmail.com>
2020-03-16 23:27:14 +0900

    timer 추가

--- StopWatch/ViewController.swift
+++ StopWatch/ViewController.swift
@@ -17,8 +17,30 @@ class ViewController: UIViewController {
     @IBOutlet weak var startButton: UIButton!
     @IBOutlet weak var ResetButton: UIButton!
     
+    var isStarting = false
+    
+    var timer = Timer()
+    var startTime = 0.0
+    var elapsed = 0.0
+    
     @IBAction func StartStop(_ sender: Any) {
+        if isStarting {
+            elapsed = Date().timeIntervalSince1970 - startTime
+            startButton.setTitle("Start", for: .normal)
+            timer.invalidate()
+        }
+        else{
+            // Timer
+            
+            startTime = Date().timeIntervalSince1970 - elapsed
+            timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(UpdateCounter), userInfo: nil, repeats: true)
+            
+             startButton.setTitle("Stop", for: .normal)
+        }
         
+        print("start \(startTime)")
+        print("start \(elapsed)")
+        isStarting = !isStarting;
     }
     
     @IBAction func Reset(_ sender: Any) {
@@ -28,6 +50,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+    
+        
         //Button
         startButton.setTitle("Start", for: .normal)
         ResetButton.setTitle("Reset", for: .normal)
@@ -36,6 +60,10 @@ class ViewController: UIViewController {
         labelMinute.text = "00"
         labelSecond.text = "00"
         labelMilieSecond.text = "00"
+    }
+    
+    @objc func UpdateCounter(){
+        
     }
 
 
